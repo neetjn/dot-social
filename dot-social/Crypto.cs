b71194edf1aac4f88ac0e3ac969c48eaa67e83f4
John Nolette <jnolette@transparent.com>
2020-01-12 15:21:01 -0500

    Cleaning up md5 hash

--- dot-social/Crypto.cs
+++ dot-social/Crypto.cs
@@ -5,12 +5,12 @@ namespace dot_social.Crypto {
   /// <summary>
   /// </summary>
   public static string MD5Hash(string input) {
-    MD5 md5Hash = MD5.Create();
-    byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
     StringBuilder sBuilder = new StringBuilder();
-    for (int i = 0; i < data.Length; i++)
-    {
-      sBuilder.Append(data[i].ToString("x2"));
+    using (MD5 md5Hash = MD5.Create()) {
+      byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+      for (int i = 0; i < data.Length; i++) {
+        sBuilder.Append(data[i].ToString("x2"));
+      }
     }
     return sBuilder.ToString();
   }
@@ -18,14 +18,14 @@ namespace dot_social.Crypto {
   /// <summary>
   /// </summary>
   public static string Sha256Hash(string input) {
-    StringBuilder Sb = new StringBuilder();
+    StringBuilder sBuilder = new StringBuilder();
     using (SHA256 hash = SHA256Managed.Create()) {
       Encoding enc = Encoding.UTF8;
       Byte[] result = hash.ComputeHash(enc.GetBytes(input));
       foreach (Byte b in result) {
-        Sb.Append(b.ToString("x2"));
+        sBuilder.Append(b.ToString("x2"));
       }
     }
-    return Sb.ToString();
+    return sBuilder.ToString();
   }
 }
